8b2478ec9223222cbd8835cc0ed73b89d2cfb25c
Queren Sales <querenhsales@gmail.com>
2025-06-05 10:29:07 -0300

    corrigindo erros de console

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -4,7 +4,8 @@ export const Avatar = ({name, imageSrc}) =>{
     return(
         <ul>
             <li>
-                <Image src={imageSrc} width={32} height={32} alt={`imagem do(a) ${name.Avatar}`} />
+                <Image src={imageSrc} width={32} height={32} 
+                alt={`imagem do(a) ${name.Avatar}`} />
             </li>
             <li>
                @{name}
