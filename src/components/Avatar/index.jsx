a5c65ebc3edc32e59b4dec8c45ffa467a0a10479
Queren Sales <querenhsales@gmail.com>
2025-06-05 10:38:17 -0300

    estilizando com module.css

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -1,7 +1,8 @@
 import Image from "next/image"
+import styles from './avatar.module.css'
 
 export const Avatar = ({name, imageSrc}) => {
-    return (<ul>
+    return (<ul className={styles.avatar}>
         <li>
             <Image src={imageSrc} width={32} height={32} alt={`Avatar do(a) ${name}`}/>
         </li>
