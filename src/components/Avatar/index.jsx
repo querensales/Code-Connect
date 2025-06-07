3630ee53ffec8d5b44ee6905782bb2329e7f0e2f
Queren Sales <querenhsales@gmail.com>
2025-06-07 13:54:12 -0300

    revisão do json

--- src/components/Avatar/index.jsx
+++ src/components/Avatar/index.jsx
@@ -2,7 +2,8 @@ import Image from "next/image"
 import styles from './avatar.module.css'
 
 export const Avatar = ({name, imageSrc}) => {
-    return (<ul className={styles.avatar}>
+    return (
+    <ul className={styles.avatar}>
         <li>
             <Image src={imageSrc} width={32} height={32} alt={`Avatar do(a) ${name}`}/>
         </li>
