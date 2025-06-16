1330366e692d652a6281723d991e0188cf3f1866
Queren Sales <querenhsales@gmail.com>
2025-06-16 13:47:55 -0300

    💄 Grid e flexbox
    corrigindo o grid da pagina

--- src/app/page.js
+++ src/app/page.js
@@ -2,11 +2,13 @@ import { CardPost } from "@/components/CardPost"
 
 async function getAllPosts() {
   const response = await fetch("http://localhost:3042/posts");
+  console.log(response);
   if (!response.ok) {
     throw new Error("Ops! Algo deu errado.");
   }
   return response.json();
 }
+
 
 export default async function Home() {
   const posts = await getAllPosts();
