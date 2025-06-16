7e5eaaa638f38587e308beecdad607678614aeb5
Queren Sales <querenhsales@gmail.com>
2025-06-16 20:54:48 -0300

    🚧 Implementa paginacao
     implementa paginação via query string e controles baseados na API

--- src/app/page.js
+++ src/app/page.js
@@ -1,26 +1,29 @@
 import { CardPost } from "@/components/CardPost"
-import styles from './page.module.css'
-import logger from "@/logger";
-import Link from 'next/link';
+import logger from "@/logger"
 
-async function getAllPosts(page) {
-  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
-  console.log(response);
+import styles from './page.module.css'
+import Link from "next/link"
+
+async function getAllPosts (page) {
+  const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`)
   if (!response.ok) {
-    logger.error("Ops! Algo deu errado.");
+    logger.error('Ops, alguma coisa correu mal')
+    return []
   }
-    logger.info('Posts obtidos com sucesso!')
-  return response.json();
+  logger.info('Posts obtidos com sucesso')
+  return response.json()
 }
 
-
-export default async function Home() {
-  const {data: posts, prev, next} = await getAllPosts(1);
+export default async function Home({ searchParams }) {
+  const currentPage = searchParams?.page || 1
+  const { data: posts, prev, next } = await getAllPosts(currentPage)
   return (
     <main className={styles.grid}>
-      {posts.map(post => <CardPost key={post.id} post={post} />)}
-      {prev && <Link href={`/?${prev}`} className={styles.prev}>Anterior</Link>}
-      {next && <Link href={`/?${next}`} className={styles.next}>Próxima página</Link>}
+      {posts.map(post =>  <CardPost key={post.id} post={post} />)}
+      <div className={styles.links}>
+        {prev && <Link href={`/?page=${prev}`}>Página anterior</Link>}
+        {next && <Link href={`/?page=${next}`}>Próxima página</Link>}
+      </div>
     </main>
-  );
+  )
 }
