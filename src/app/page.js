0cbca88ec30babcb3dde00399a68742e99b509a5
Queren Sales <querenhsales@gmail.com>
2025-06-16 20:30:35 -0300

    🚧 Adicionando paginacao (link)
    adicionando codigo de paginacao

--- src/app/page.js
+++ src/app/page.js
@@ -1,6 +1,7 @@
 import { CardPost } from "@/components/CardPost"
 import styles from './page.module.css'
 import logger from "@/logger";
+import Link from 'next/link';
 
 async function getAllPosts(page) {
   const response = await fetch(`http://localhost:3042/posts?_page=${page}&_per_page=6`);
@@ -14,10 +15,12 @@ async function getAllPosts(page) {
 
 
 export default async function Home() {
-  const {data: posts} = await getAllPosts(1);
+  const {data: posts, prev, next} = await getAllPosts(1);
   return (
     <main className={styles.grid}>
       {posts.map(post => <CardPost key={post.id} post={post} />)}
+      {prev && <Link href={`/?${prev}`} className={styles.prev}>Anterior</Link>}
+      {next && <Link href={`/?${next}`} className={styles.next}>Próxima página</Link>}
     </main>
   );
 }
